cd7f672727af5c99bb9b1c7f4cc57d6a699f354e
Eduard Anton <eduard.anton@mail.mcgill.ca>
2024-01-20 14:33:32 -0500

    New docker instructions in README

--- src/app.py
+++ src/app.py
@@ -4,10 +4,13 @@ import pymongo
 app = Flask(__name__)
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["mydatabase"]
+users = db["users"]
 
 @app.route('/')
 def home():
-    return client.list_database_names()
+    mydict = { "name": "John", "address": "Highway 37" }
+    x = users.insert_one(mydict)
+    return str(x.inserted_id)
 
 
 app.run(host='0.0.0.0', port=8080)
