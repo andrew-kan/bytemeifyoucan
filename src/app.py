9c2d4e95b4508b670af4d86c27534bef953dd367
Eduard Anton <eduard.anton@mail.mcgill.ca>
2024-01-20 15:19:04 -0500

    Added test controller and services

--- src/app.py
+++ src/app.py
@@ -1,16 +1,28 @@
 from flask import Flask
-import pymongo
+from flask import request
+from db import Repository
 
 app = Flask(__name__)
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["mydatabase"]
-users = db["users"]
+db = Repository("mongodb://localhost:27017/")
 
 @app.route('/')
 def home():
-    mydict = { "name": "John", "address": "Highway 37" }
-    x = users.insert_one(mydict)
-    return str(x.inserted_id)
+    return "Hello world"
 
+@app.route('/user/new')
+def new_user():
+    user = db.create_user("John doe", "johndoe@gmail.com")
+    if user == None:
+        return "User already exists", 403
+    else:
+        print(user)
+        return f"User was created with success."
+
+@app.route('/user/exists')
+def user_exists():
+    email = request.args.get('email', default = "", type = str)
+    if not db.user_exists(email):
+        return "Not found", 404
+    return email
 
 app.run(host='0.0.0.0', port=8080)
