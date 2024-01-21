0d36b0f567986b25ec15b19cd65cafa0dff11ba5
Lancelot Normand <lancelotnormand@outlook.com>
2024-01-21 03:28:34 -0500

    better tiles

--- infocus/src/app/important-tiles/important-tiles.component.ts
+++ infocus/src/app/important-tiles/important-tiles.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { AvatarComponent } from '../avatar/avatar.component';
+import { DialogOverviewExample } from '../example/dialog-overview-example';
+import {MatButtonModule} from '@angular/material/button';
+
 @Component({
   selector: 'app-important-tiles',
   standalone: true,
-  imports: [AvatarComponent],
+  imports: [AvatarComponent,MatButtonModule, DialogOverviewExample],
   templateUrl: './important-tiles.component.html',
   styleUrl: './important-tiles.component.css'
 })
