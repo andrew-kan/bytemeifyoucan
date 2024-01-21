0f67c806bf9fd5406ae95aae7c3037e47ec9256e
Andrew <andrew.m.kan@gmail.com>
2024-01-20 23:40:45 -0500

    add gridding to home

--- infocus/src/app/grid/grid.component.ts
+++ infocus/src/app/grid/grid.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ImportantTilesComponent } from '../important-tiles/important-tiles.component';
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [ImportantTilesComponent],
+  imports: [ImportantTilesComponent, CommonModule],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.css'
 })
 export class GridComponent {
-
+  tiles = Array(9).fill(null); // Replace with your actual tile data
 }
--- infocus/src/app/side-panel/side-panel.component.ts
+++ infocus/src/app/side-panel/side-panel.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NoticeTilesComponent } from '../notice-tiles/notice-tiles.component';
 @Component({
   selector: 'app-side-panel',
   standalone: true,
-  imports: [NoticeTilesComponent],
+  imports: [NoticeTilesComponent, CommonModule],
   templateUrl: './side-panel.component.html',
   styleUrl: './side-panel.component.css'
 })
 export class SidePanelComponent {
+  tiles = Array(9).fill(null); // Replace with your actual tile data
 
 }
