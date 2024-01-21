35581295a8ac7c675e717097463d318d0556f59c
Lancelot Normand <lancelotnormand@outlook.com>
2024-01-21 10:54:37 -0500

    dd

--- new-front-end/src/app/important-tiles/important-tiles.component.ts
+++ new-front-end/src/app/important-tiles/important-tiles.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { AvatarComponent } from '../avatar/avatar.component';
 import { DialogOverviewExample } from '../example/dialog-overview-example';
 import {MatButtonModule} from '@angular/material/button';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-important-tiles',
   standalone: true,
-  imports: [AvatarComponent,MatButtonModule, DialogOverviewExample],
+  imports: [AvatarComponent,MatButtonModule, DialogOverviewExample,],
   templateUrl: './important-tiles.component.html',
   styleUrl: './important-tiles.component.css'
 })
@@ -15,6 +16,8 @@ export class ImportantTilesComponent {
   @Input() subject!:string
   @Input() email!:any;
 
+  constructor(private emailService:EmailService){}
+
 
   extractName(email: string): string {
     const match = email.match(/^(.*)\s<.*>$/);
@@ -22,6 +25,13 @@ export class ImportantTilesComponent {
       return match[1]; // This will be "Byteme Test"
     }
     return email; // Return the original string if no match is found
+  }
+
+
+  sendMail(){
+    this.emailService.send_reply(this.email, this.email["reply"][0]["reply"]).subscribe(response => {
+      console.log("called")
+    })
   }
   
 }
