8401fb5b941ea8508ac246e77f6da9c086a2c735
Lancelot Normand <lancelotnormand@outlook.com>
2024-01-21 02:31:27 -0500

    dialog window

--- infocus/src/app/example/dialog-overview-example.ts
+++ infocus/src/app/example/dialog-overview-example.ts
@@ -16,6 +16,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 export interface DialogData {
   animal: string;
   name: string;
+  response:string;
 }
 
 /**
@@ -25,18 +26,21 @@ export interface DialogData {
   selector: 'dialog-overview-example',
   templateUrl: 'dialog-overview-example.html',
   standalone: true,
+  styleUrl:'dialog-overview-example.css',
   imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule],
 })
 export class DialogOverviewExample {
-  animal: string;
-  name: string;
+  animal = "ddddd";
+  name = "fddd";
 
   constructor(public dialog: MatDialog) {}
 
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
-      data: {name: this.name, animal: this.animal},
-    });
+        width: '800px', // Set the width you want
+        data: {name: 'User', response: ''}
+      });
+      
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
@@ -70,3 +74,8 @@ export class DialogOverviewExampleDialog {
     this.dialogRef.close();
   }
 }
+
+
+/**  Copyright 2024 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
--- infocus/src/main.ts
+++ infocus/src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './app/home-page/home-page.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+bootstrapApplication(HomePageComponent, {
+  providers: [
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatNativeDateModule,
+  ]
+}).catch(err => console.error(err));
